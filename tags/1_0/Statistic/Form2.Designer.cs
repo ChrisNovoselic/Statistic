ce8e17685ef1abb94689e2a0b5c1c6e90402c3af
chadchaning@gmail.com <chadchaning@gmail.com@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2013-04-10 02:39:42 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@38 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ tags/1_0/Statistic/Form2.Designer.cs
@@ -0,0 +1,295 @@
+namespace Statistic
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwMT = new System.Windows.Forms.DataGridView();
+            this.dgwM = new System.Windows.Forms.DataGridView();
+            this.Name0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgwH = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Object = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ParNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgwHT = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwHT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwMT
+            // 
+            this.dgwMT.AllowUserToAddRows = false;
+            this.dgwMT.AllowUserToDeleteRows = false;
+            this.dgwMT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwMT.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name1,
+            this.Object,
+            this.Name2,
+            this.Item,
+            this.ParNumber,
+            this.Value0,
+            this.Date,
+            this.Id});
+            this.dgwMT.Location = new System.Drawing.Point(1, 1);
+            this.dgwMT.Name = "dgwMT";
+            this.dgwMT.ReadOnly = true;
+            this.dgwMT.Size = new System.Drawing.Size(916, 209);
+            this.dgwMT.TabIndex = 0;
+            // 
+            // dgwM
+            // 
+            this.dgwM.AllowUserToAddRows = false;
+            this.dgwM.AllowUserToDeleteRows = false;
+            this.dgwM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwM.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name0,
+            this.Value});
+            this.dgwM.Location = new System.Drawing.Point(1, 447);
+            this.dgwM.Name = "dgwM";
+            this.dgwM.ReadOnly = true;
+            this.dgwM.Size = new System.Drawing.Size(367, 137);
+            this.dgwM.TabIndex = 2;
+            // 
+            // Name
+            // 
+            this.Name0.HeaderText = "Name";
+            this.Name0.Name = "Name";
+            this.Name0.ReadOnly = true;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            // 
+            // dgwH
+            // 
+            this.dgwH.AllowUserToAddRows = false;
+            this.dgwH.AllowUserToDeleteRows = false;
+            this.dgwH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwH.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dgwH.Location = new System.Drawing.Point(374, 447);
+            this.dgwH.Name = "dgwH";
+            this.dgwH.ReadOnly = true;
+            this.dgwH.Size = new System.Drawing.Size(367, 137);
+            this.dgwH.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Value";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "Name1";
+            this.Name1.Name = "Name1";
+            this.Name1.ReadOnly = true;
+            // 
+            // Object
+            // 
+            this.Object.HeaderText = "Object";
+            this.Object.Name = "Object";
+            this.Object.ReadOnly = true;
+            // 
+            // Name2
+            // 
+            this.Name2.HeaderText = "Name2";
+            this.Name2.Name = "Name2";
+            this.Name2.ReadOnly = true;
+            // 
+            // Item
+            // 
+            this.Item.HeaderText = "Item";
+            this.Item.Name = "Item";
+            this.Item.ReadOnly = true;
+            // 
+            // ParNumber
+            // 
+            this.ParNumber.HeaderText = "ParNumber";
+            this.ParNumber.Name = "ParNumber";
+            this.ParNumber.ReadOnly = true;
+            // 
+            // Value0
+            // 
+            this.Value0.HeaderText = "Value0";
+            this.Value0.Name = "Value0";
+            this.Value0.ReadOnly = true;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            // 
+            // dgwHT
+            // 
+            this.dgwHT.AllowUserToAddRows = false;
+            this.dgwHT.AllowUserToDeleteRows = false;
+            this.dgwHT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwHT.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10});
+            this.dgwHT.Location = new System.Drawing.Point(1, 216);
+            this.dgwHT.Name = "dgwHT";
+            this.dgwHT.ReadOnly = true;
+            this.dgwHT.Size = new System.Drawing.Size(916, 209);
+            this.dgwHT.TabIndex = 4;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Name1";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Object";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "Name2";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "Item";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.HeaderText = "ParNumber";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.HeaderText = "Value0";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.HeaderText = "Date";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.ReadOnly = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 629);
+            this.Controls.Add(this.dgwHT);
+            this.Controls.Add(this.dgwH);
+            this.Controls.Add(this.dgwM);
+            this.Controls.Add(this.dgwMT);
+            this.Name = "Form2";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwHT)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwMT;
+        private System.Windows.Forms.DataGridView dgwM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridView dgwH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Object;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ParNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridView dgwHT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+    }
+}
---
+++ tags/1_0/Statistic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Statistic
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            InitTEC init = new InitTEC();
+
+            Application.Run(new MainForm(init.tec));
+        }
+    }
+}
