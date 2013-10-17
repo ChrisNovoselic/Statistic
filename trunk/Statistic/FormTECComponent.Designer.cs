f4e0901fd161b86ca083d0f683b31dabfd308d75
chadchaning@gmail.com <chadchaning@gmail.com@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2013-10-17 09:57:34 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@98 d8fff809-da14-dc41-87d1-cc2ed163d9e8

--- trunk/Statistic/FormTECComponent.Designer.cs
+++ trunk/Statistic/FormTECComponent.Designer.cs
@@ -28,76 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBoxItem = new System.Windows.Forms.ListBox();
-            this.dataGridViewProp = new System.Windows.Forms.DataGridView();
-            this.ColumnTG = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColumnProp1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.ColumnProp2 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.listBoxTEC = new System.Windows.Forms.ListBox();
             this.comboBoxMode = new System.Windows.Forms.ComboBox();
             this.buttonAdd = new System.Windows.Forms.Button();
             this.buttonDel = new System.Windows.Forms.Button();
             this.buttonOk = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProp)).BeginInit();
+            this.listBoxItem = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBoxTG = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // listBoxItem
+            // listBoxTEC
             // 
-            this.listBoxItem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.listBoxTEC.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.listBoxItem.FormattingEnabled = true;
-            this.listBoxItem.Location = new System.Drawing.Point(12, 52);
-            this.listBoxItem.Name = "listBoxItem";
-            this.listBoxItem.Size = new System.Drawing.Size(136, 160);
-            this.listBoxItem.TabIndex = 0;
-            // 
-            // dataGridViewProp
-            // 
-            this.dataGridViewProp.AllowUserToOrderColumns = true;
-            this.dataGridViewProp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridViewProp.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            this.dataGridViewProp.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridViewProp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewProp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ColumnTG,
-            this.ColumnProp1,
-            this.ColumnProp2});
-            this.dataGridViewProp.Location = new System.Drawing.Point(163, 13);
-            this.dataGridViewProp.Name = "dataGridViewProp";
-            this.dataGridViewProp.Size = new System.Drawing.Size(264, 225);
-            this.dataGridViewProp.TabIndex = 3;
-            // 
-            // ColumnTG
-            // 
-            this.ColumnTG.Frozen = true;
-            this.ColumnTG.HeaderText = "Эн./блок";
-            this.ColumnTG.Name = "ColumnTG";
-            this.ColumnTG.ReadOnly = true;
-            this.ColumnTG.Width = 77;
-            // 
-            // ColumnProp1
-            // 
-            this.ColumnProp1.Frozen = true;
-            this.ColumnProp1.HeaderText = "ГТП";
-            this.ColumnProp1.Name = "ColumnProp1";
-            this.ColumnProp1.ReadOnly = true;
-            this.ColumnProp1.Width = 38;
-            // 
-            // ColumnProp2
-            // 
-            this.ColumnProp2.Frozen = true;
-            this.ColumnProp2.HeaderText = "ЩУ";
-            this.ColumnProp2.Name = "ColumnProp2";
-            this.ColumnProp2.ReadOnly = true;
-            this.ColumnProp2.Width = 38;
+            this.listBoxTEC.FormattingEnabled = true;
+            this.listBoxTEC.Location = new System.Drawing.Point(12, 13);
+            this.listBoxTEC.Name = "listBoxTEC";
+            this.listBoxTEC.Size = new System.Drawing.Size(136, 199);
+            this.listBoxTEC.TabIndex = 0;
             // 
             // comboBoxMode
             // 
             this.comboBoxMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxMode.FormattingEnabled = true;
-            this.comboBoxMode.Location = new System.Drawing.Point(12, 13);
+            this.comboBoxMode.Location = new System.Drawing.Point(164, 12);
             this.comboBoxMode.Name = "comboBoxMode";
             this.comboBoxMode.Size = new System.Drawing.Size(136, 21);
             this.comboBoxMode.TabIndex = 4;
@@ -126,7 +85,7 @@
             // buttonOk
             // 
             this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonOk.Location = new System.Drawing.Point(240, 251);
+            this.buttonOk.Location = new System.Drawing.Point(271, 251);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(89, 23);
             this.buttonOk.TabIndex = 7;
@@ -137,7 +96,7 @@
             // buttonCancel
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonCancel.Location = new System.Drawing.Point(338, 251);
+            this.buttonCancel.Location = new System.Drawing.Point(369, 251);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(89, 23);
             this.buttonCancel.TabIndex = 8;
@@ -145,40 +104,106 @@
             this.buttonCancel.UseVisualStyleBackColor = true;
             this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
+            // listBoxItem
+            // 
+            this.listBoxItem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxItem.FormattingEnabled = true;
+            this.listBoxItem.Location = new System.Drawing.Point(164, 39);
+            this.listBoxItem.Name = "listBoxItem";
+            this.listBoxItem.Size = new System.Drawing.Size(136, 173);
+            this.listBoxItem.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(236, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(164, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(391, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(319, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(66, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // listBoxTG
+            // 
+            this.listBoxTG.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxTG.FormattingEnabled = true;
+            this.listBoxTG.Location = new System.Drawing.Point(319, 12);
+            this.listBoxTG.Name = "listBoxTG";
+            this.listBoxTG.Size = new System.Drawing.Size(136, 199);
+            this.listBoxTG.TabIndex = 12;
+            // 
             // FormTECComponent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 282);
+            this.ClientSize = new System.Drawing.Size(467, 282);
             this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBoxTG);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBoxItem);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.buttonDel);
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.comboBoxMode);
-            this.Controls.Add(this.dataGridViewProp);
-            this.Controls.Add(this.listBoxItem);
+            this.Controls.Add(this.listBoxTEC);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormTECComponent";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Настройка состава ТЭЦ, ГТП, ЩУ";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProp)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBoxItem;
-        private System.Windows.Forms.DataGridView dataGridViewProp;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTG;
-        private System.Windows.Forms.DataGridViewComboBoxColumn ColumnProp1;
-        private System.Windows.Forms.DataGridViewComboBoxColumn ColumnProp2;
+        private System.Windows.Forms.ListBox listBoxTEC;
         private System.Windows.Forms.ComboBox comboBoxMode;
         private System.Windows.Forms.Button buttonAdd;
         private System.Windows.Forms.Button buttonDel;
         private System.Windows.Forms.Button buttonOk;
         private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ListBox listBoxItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBoxTG;
     }
 }
