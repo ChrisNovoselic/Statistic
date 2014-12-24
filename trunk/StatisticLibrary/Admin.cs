bc26357e52928413767481d76c39af693fba70e9
ChrjapinAN <ChrjapinAN@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2014-12-24 03:04:51 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@481 d8fff809-da14-dc41-87d1-cc2ed163d9e8

--- trunk/StatisticLibrary/Admin.cs
+++ trunk/StatisticLibrary/Admin.cs
@@ -15,9 +15,7 @@ using HClassLibrary;
 
 namespace StatisticCommon
 {
-    public delegate void DelegateDateFunc(DateTime date);
-
-    public abstract class HAdmin : object
+    public abstract class HAdmin : HStates
     {
         public static int SEASON_BASE = 5;
         public enum seasonJumpE
@@ -65,13 +63,6 @@ namespace StatisticCommon
         public volatile RDGStruct[] m_prevRDGValues;
         public RDGStruct[] m_curRDGValues;
 
-        protected DelegateFunc delegateStartWait;
-        protected DelegateFunc delegateStopWait;
-        protected DelegateFunc delegateEventUpdate;
-
-        protected DelegateFunc errorReport;
-        protected DelegateFunc actionReport;
-
         protected DelegateFunc saveComplete = null;
         protected DelegateDateFunc readyData = null;
         protected DelegateFunc errorData = null;
@@ -91,10 +82,6 @@ namespace StatisticCommon
         /// </summary>
         public int indxTECComponents;
         /// <summary>
-        /// Текущий идентификатор соединения с БД (для сохранения между вызовами функций)
-        /// </summary>
-        protected int m_IdListenerCurrent;
-        /// <summary>
         /// Хранения значений дыты/времени
         /// </summary>
         public DateTime m_prevDate
@@ -110,24 +97,6 @@ namespace StatisticCommon
         /// </summary>
         protected int[,] m_arHaveDates;
         protected int m_iHavePBR_Number;
-        /// <summary>
-        /// Объект для синхронизации изменения списка состояний
-        /// </summary>
-        protected Object m_lockState;
-
-        protected Thread taskThread;
-        protected Semaphore semaState;
-        public enum INDEX_WAITHANDLE_REASON { SUCCESS, ERROR, BREAK, COUNT_INDEX_WAITHANDLE_REASON }
-        protected WaitHandle [] m_waitHandleState;
-        //protected AutoResetEvent evStateEnd;
-        public volatile int threadIsWorking;
-        protected volatile bool newState;
-        protected volatile List<int /*StatesMachine*/> states;
-
-        protected Dictionary <int, int []> m_dictIdListeners;
-
-        private bool actived;
-        public bool m_bIsActive { get { return actived; } }
 
         private static int m_iSeasonAction;
         public static int SeasonAction {
@@ -147,30 +116,20 @@ namespace StatisticCommon
 
         public HAdmin()
         {
-            m_IdListenerCurrent = -1;
-
-            m_dictIdListeners = new Dictionary<int,int[]> ();
 
             m_iHavePBR_Number = -1;
-
-            //m_iSeasonHour = -1;
 
             Initialize ();
         }
 
-        protected virtual void Initialize () {
-            actived = false;
-            threadIsWorking = -1;
+        protected override void Initialize () {
+            base.Initialize ();
 
             using_date = false;
             m_ignore_date = false;
             //m_ignore_connsett_data = false;
 
             m_arHaveDates = new int[(int)CONN_SETT_TYPE.PBR + 1, 24];
-
-            m_lockState = new Object();
-
-            states = new List<int /*StatesMachine*/>();
 
             allTECComponents = new List<TECComponent>();
 
@@ -294,26 +253,12 @@ namespace StatisticCommon
 
         public abstract bool WasChanged();
 
-        public virtual void Activate(bool active)
-        {
-            if (active == true) threadIsWorking++; else ;
-
-            if (actived == active)
-            {
-                return ;
-            }
-            else
-            {
-                actived = active;
-            }
-        }
-
         private void register(int id, ConnectionSettings connSett, string name, CONN_SETT_TYPE type)
         {
-            m_dictIdListeners[id][(int)type] = DbSources.Sources().Register(connSett, true, @"ТЭЦ=" + name + @", DESC=" + type.ToString());
+            register (id, connSett, name, (int)type);
         }
 
-        public void StartDbInterfaces()
+        public override void StartDbInterfaces()
         {
             if (!(m_list_tec == null))
                 foreach (TEC t in m_list_tec)
@@ -358,29 +303,24 @@ namespace StatisticCommon
                 Logging.Logg().Error(@"HAdmin::StartDbInterfaces () - m_list_tec == null ...");
         }
 
-        private void stopDbInterfaces()
-        {
-            if (!(m_list_tec == null))
-                foreach (TEC t in m_list_tec)
-                    for (int i = (int)CONN_SETT_TYPE.ADMIN; i < (int)CONN_SETT_TYPE.COUNT_CONN_SETT_TYPE; i++)
-                    {
-                        if ((m_dictIdListeners.ContainsKey (t.m_id) == true) && (!(m_dictIdListeners[t.m_id][i] < 0)))
-                        {
-                            DbSources.Sources().UnRegister(m_dictIdListeners[t.m_id][i]);
-                            m_dictIdListeners[t.m_id][i] = -1;
-                        }
-                        else
-                            ;
-                    }
-            else
-                //Вообще нельзя что-либо инициализировать
-                Logging.Logg().Error(@"HAdmin::stopDbInterfaces () - m_list_tec == null ...");
-        }
-
-        public void StopDbInterfaces()
-        {
-            stopDbInterfaces();
-        }
+        //private void stopDbInterfaces()
+        //{
+        //    if (!(m_list_tec == null))
+        //        foreach (TEC t in m_list_tec)
+        //            for (int i = (int)CONN_SETT_TYPE.ADMIN; i < (int)CONN_SETT_TYPE.COUNT_CONN_SETT_TYPE; i++)
+        //            {
+        //                if ((m_dictIdListeners.ContainsKey (t.m_id) == true) && (!(m_dictIdListeners[t.m_id][i] < 0)))
+        //                {
+        //                    DbSources.Sources().UnRegister(m_dictIdListeners[t.m_id][i]);
+        //                    m_dictIdListeners[t.m_id][i] = -1;
+        //                }
+        //                else
+        //                    ;
+        //            }
+        //    else
+        //        //Вообще нельзя что-либо инициализировать
+        //        Logging.Logg().Error(@"HAdmin::stopDbInterfaces () - m_list_tec == null ...");
+        //}
 
         public void RefreshConnectionSettings()
         {
@@ -400,19 +340,6 @@ namespace StatisticCommon
                 ;
         }
 
-        public void SetDelegateWait(DelegateFunc dStart, DelegateFunc dStop, DelegateFunc dStatus)
-        {
-            this.delegateStartWait = dStart;
-            this.delegateStopWait = dStop;
-            this.delegateEventUpdate = dStatus;
-        }
-
-        public void SetDelegateReport(DelegateFunc ferr, DelegateFunc fact)
-        {
-            this.errorReport = ferr;
-            this.actionReport = fact;
-        }
-
         public void SetDelegateSaveComplete(DelegateFunc f) {            
             saveComplete = f;
 
@@ -424,16 +351,9 @@ namespace StatisticCommon
         //public void SetDelegateTECComponent(DelegateFunc f) { fillTECComponent = f; }
 
         public void SetDelegateDatetime(DelegateDateFunc f) { setDatetime = f; }
-
-        protected void MessageBox(string msg, MessageBoxButtons btn = MessageBoxButtons.OK, MessageBoxIcon icon = MessageBoxIcon.Error)
-        {
-            //MessageBox.Show(this, msg, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-            Logging.Logg().Error(msg);
-        }
-
+ 
         //public virtual void ClearValues(int cnt = -1) {
-        public virtual void ClearValues()
+        public override void ClearValues()
         {
             int cntHours = 24;
 
@@ -597,212 +517,17 @@ namespace StatisticCommon
             return iNum;
         }
 
-        protected virtual void InitializeSyncState ()
-        {
-            if (m_waitHandleState == null)
-                m_waitHandleState = new WaitHandle [1];
-            else
-                ;
-
-            m_waitHandleState [(int)INDEX_WAITHANDLE_REASON.SUCCESS] = new AutoResetEvent(true);
-        }
-
-        //protected abstract bool InitDbInterfaces ();
-
-        public void Request(int idListener, string request)
-        {
-            DbSources.Sources().Request(m_IdListenerCurrent = idListener, request);
-        }
-
-        public virtual bool Response(int idListener, out bool error, out DataTable table/*, bool bIsTec*/)
-        {
-            return DbSources.Sources().Response(idListener, out error, out table);
-        }
-
-        public virtual bool Response(out bool error, out DataTable table/*, bool bIsTec*/)
-        {
-            return DbSources.Sources().Response(m_IdListenerCurrent, out error, out table);
-        }
-
-        protected abstract bool StateRequest(int /*StatesMachine*/ state);
-
-        protected abstract bool StateCheckResponse(int /*StatesMachine*/ state, out bool error, out DataTable table);
-
-        protected abstract bool StateResponse(int /*StatesMachine*/ state, DataTable table);
-
-        protected abstract void StateErrors(int /*StatesMachine*/ state, bool response);
-
-        public virtual void Start()
-        {
-            threadIsWorking = 0;
-            taskThread = new Thread(new ParameterizedThreadStart(TecView_ThreadFunction));
-            taskThread.Name = "Интерфейс к РДГ";
-            taskThread.IsBackground = true;
-
-            semaState = new Semaphore(1, 1);
-
-            InitializeSyncState ();
-
-            semaState.WaitOne();
-            taskThread.Start();
-        }
-
-        public void ClearStates()
+        public override void ClearStates()
         {
             //lock (m_lockState)
             //{
-                newState = true;
-                states.Clear ();
+                base.ClearStates();
 
                 if (!(FormMainBaseWithStatusStrip.m_report == null))
                     FormMainBaseWithStatusStrip.m_report.ClearStates();
                 else
                     Logging.Logg().Error(@"HAdmin::ClearStates () - m_report=null");
             //}
-        }
-
-        public virtual void Stop()
-        {
-            bool joined;
-            threadIsWorking = -1;
-
-            StopDbInterfaces ();
-            
-            ClearStates ();
-
-            if ((!(taskThread == null)) && taskThread.IsAlive)
-            {
-                try { semaState.Release(1); }
-                catch (Exception e)
-                {
-                    Logging.Logg().Exception(e, "HAdmin::StopThreadSourceData () - semaState.Release(1)");
-                }
-
-                joined = taskThread.Join(666);
-                if (joined == false)
-                    taskThread.Abort();
-                else
-                    ;
-            }
-            else ;
-        }
-
-        protected void TecView_ThreadFunction(object data)
-        {
-            int index;
-            int /*StatesMachine*/ currentState;
-
-            while (! (threadIsWorking < 0))
-            {
-                semaState.WaitOne();
-
-                index = 0;
-
-                lock (m_lockState)
-                {
-                    if (states.Count == 0)
-                        continue;
-                    else
-                        ;
-
-                    currentState = states[index];
-                    newState = false;
-                }
-
-                while (true)
-                {
-                    bool requestIsOk = true;
-                    bool error = true;
-                    bool dataPresent = false;
-                    DataTable table = null;
-                    for (int i = 0; i < DbInterface.MAX_RETRY && !dataPresent && !newState; i++)
-                    {
-                        if (error)
-                        {
-                            requestIsOk = StateRequest(currentState);
-                            if (!requestIsOk)
-                                break;
-                            else
-                                ;
-                        }
-                        else
-                            ;
-
-                        error = false;
-                        for (int j = 0; j < DbInterface.MAX_WAIT_COUNT && !dataPresent && !error && !newState; j++)
-                        {
-                            System.Threading.Thread.Sleep(DbInterface.WAIT_TIME_MS);
-                            dataPresent = StateCheckResponse(currentState, out error, out table);
-                        }
-                    }
-
-                    if (requestIsOk)
-                    {
-                        bool responseIsOk = true;
-                        if ((dataPresent == true) && (error == false) && (newState == false))
-                            responseIsOk = StateResponse(currentState, table);
-                        else
-                            ;
-
-                        if (((responseIsOk == false) || (dataPresent == false) || (error == true)) && (newState == false))
-                        {
-                            StateErrors(currentState, !responseIsOk);
-                            lock (m_lockState)
-                            {
-                                if (newState == false)
-                                {
-                                    states.Clear();
-                                    break;
-                                }
-                                else
-                                    ;
-                            }
-                        }
-                        else
-                            ;
-                    }
-                    else
-                    {
-                        lock (m_lockState)
-                        {
-                            if (newState == false)
-                            {
-                                states.Clear();
-                                break;
-                            }
-                            else
-                                ;
-                        }
-                    }
-
-                    index++;
-
-                    lock (m_lockState)
-                    {
-                        if (index == states.Count)
-                            break;
-                        else
-                            ;
-
-                        if (newState)
-                            break;
-                        else
-                            ;
-                        currentState = states[index];
-                    }
-                }
-
-                //Закончена обработка всех событий
-                completeHandleStates();
-            }
-            try
-            {
-                semaState.Release(1);
-            }
-            catch (Exception e)
-            { //System.Threading.SemaphoreFullException
-                Logging.Logg().Exception(e, "HAdmin::TecView_ThreadFunction () - semaState.Release(1)");
-            }
         }
 
         /// <summary>
@@ -889,28 +614,6 @@ namespace StatisticCommon
                 ;
         }
 
-        protected void GetCurrentTimeRequest(DbInterface.DB_TSQL_INTERFACE_TYPE typeDB, int idListatener)
-        {
-            string query = string.Empty;
-
-            switch (typeDB)
-            {
-                case DbInterface.DB_TSQL_INTERFACE_TYPE.MySQL:
-                    query = @"SELECT now()";
-                    break;
-                case DbInterface.DB_TSQL_INTERFACE_TYPE.MSSQL:
-                    query = @"SELECT GETDATE()";
-                    break;
-                default:
-                    break;
-            }
-
-            if (query.Equals(string.Empty) == false)
-                Request(idListatener, query);
-            else
-                ;
-        }
-
         protected bool IsCanUseTECComponents()
         {
             //bool bRes = false;
@@ -920,12 +623,7 @@ namespace StatisticCommon
 
         public virtual void AbortThreadRDGValues(INDEX_WAITHANDLE_REASON reason)
         {
-            if (m_waitHandleState.Length > (int)reason)
-            {
-                ((ManualResetEvent)m_waitHandleState[(int)reason]).Set();
-            }
-            else
-                ;
+            abortThreadGetValues(reason);
         }
 
         protected bool IsHaveDates(CONN_SETT_TYPE type, int indx)
@@ -1041,72 +739,6 @@ namespace StatisticCommon
             strRes += @":00";
 
             return strRes;
-        }
-
-        private static string s_Name_Moscow_TimeZone = @"Russian Standard Time";
-        public static DateTime ToMoscowTimeZone(DateTime dt)
-        //public static DateTime ToCurrentTimeZone(DateTime dt, int offset_msc)
-        {
-            DateTime dtRes;
-
-            if (! (dt.Kind == DateTimeKind.Local)) {
-            //    dtRes = TimeZoneInfo.ConvertTimeFromUtc(dt, TimeZoneInfo.FindSystemTimeZoneById(s_Name_Moscow_TimeZone));
-                dtRes = dt.Add(GetUTCOffsetOfMoscowTimeZone ());
-            } else {
-                dtRes = dt - TimeZoneInfo.Local.GetUtcOffset (dt);
-                if (dtRes.IsDaylightSavingTime () == true) {
-                    dtRes = dtRes.AddHours(-1);
-                } else { }
-
-                dtRes = dtRes.Add(GetUTCOffsetOfMoscowTimeZone());
-            //    //dtRes = dtRes.Add(GetUTCOffsetOfCurrentTimeZone(offset_msc));
-            }
-
-            return dtRes;
-        }
-
-        //public static TimeSpan GetOffsetOfCurrentTimeZone()
-        //{
-        //    return DateTime.Now - HAdmin.ToCurrentTimeZone(TimeZone.CurrentTimeZone.ToUniversalTime(DateTime.Now));
-        //}
-
-        public static TimeSpan GetUTCOffsetOfMoscowTimeZone()
-        {
-            ////Перечисление всех зо ОС
-            //System.Collections.ObjectModel.ReadOnlyCollection <TimeZoneInfo> tzi = TimeZoneInfo.GetSystemTimeZones ();
-            //foreach (TimeZoneInfo tz in tzi) {
-            //    Console.WriteLine (tz.DisplayName + @", " +  tz.StandardName + @", " + tz.Id);
-            //}
-
-            ////Вариант №1 - работает, если у пользователя установлено обновление (сезонный переход 26.10.2014)
-            //return TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Now, HAdmin.s_Name_Moscow_TimeZone) - DateTime.UtcNow;
-            ////Вариант №2 - работает, если у пользователя установлено обновление (сезонный переход 26.10.2014)
-            //return TimeZoneInfo.FindSystemTimeZoneById(HAdmin.s_Name_Moscow_TimeZone).GetUtcOffset(DateTime.Now);
-            ////Вариант №3 - работает, если у пользователя установлено обновление (сезонный переход 26.10.2014) + известно смещение зоны пользователя от МСК
-            //return DateTime.UtcNow - DateTime.Now - TimeSpan.FromHours(offset_msc);
-            //Вариант №4
-            return TimeSpan.FromHours (3);
-        }
-
-        public void ErrorReport (string msg) {
-            if (!(errorReport == null))
-            {
-                FormMainBaseWithStatusStrip.m_report.ErrorReport (msg);
-                errorReport ();
-            }
-            else
-                ;
-        }
-
-        public void ActionReport(string msg)
-        {
-            if (! (actionReport == null))
-            {
-                FormMainBaseWithStatusStrip.m_report.ActionReport(msg);
-                actionReport();
-            }
-            else
-                ;
         }
 
         public static int CountHoursOfDate (DateTime dtReq) {
