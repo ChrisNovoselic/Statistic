8e1b164b0ff1fb7918108761a34b860b67127751
ChrjapinAN <ChrjapinAN@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2015-04-30 08:44:15 +0000

    1.9.5.9 Стабильная версия
    git-svn-id: svn:///U:/Repos/statistic-techsite@589 d8fff809-da14-dc41-87d1-cc2ed163d9e8

--- trunk/Statistic/Properties/AssemblyInfo.cs
+++ trunk/Statistic/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.9.5.8")]
-[assembly: AssemblyFileVersion("1.9.5.8")]
+[assembly: AssemblyVersion("1.9.5.9")]
+[assembly: AssemblyFileVersion("1.9.5.9")]
