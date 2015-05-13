498458e2ac1575dd21b2a2f7d2e15c63a0f46afd
PasternakAS <PasternakAS@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2015-05-13 10:22:58 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@602 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ trunk/StatisticDiagnostic/FormStatisticDiagnostic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using HClassLibrary;
+using System.Windows.Forms;
+
+
+namespace StatisticDiagnostic
+{
+    public partial class FormStatisticDiagnostic : Form
+    {
+        public FormStatisticDiagnostic()
+        {
+            InitializeComponent();
+        }
+
+        private void FormStatisticDiagnostic_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void FormStatisticDiagnostic_Activate(object obj, EventArgs ev)
+        {
+            panelMain.Activate(true);
+        }
+
+        private void FormStatisticDiagnostic_Deactivate(object obj, EventArgs ev)
+        {
+            panelMain.Activate(false);
+        }
+                            
+    }
+}
