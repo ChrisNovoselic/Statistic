7675a131d6d3c38930d23fad46756396cf40f5f1
pasternakas <pasternakas@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2015-10-08 07:00:17 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@736 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/diagnostic_006/StatisticDiagnostic1/FormDiagnostic.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace StatisticDiagnostic1
+{
+    partial class FormDiagnostic
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        PanelStatisticDiagnostic1 panelMain;
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelMain = new StatisticDiagnostic1.PanelStatisticDiagnostic1(this.components);
+            this.SuspendLayout();
+            // 
+            // panelMain
+            //
+            this.panelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.panelMain.Size = new System.Drawing.Size(1041, 600);
+            this.panelMain.TabIndex = 0;
+            
+            // 
+            // FormDiagnostic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 600);
+            this.Controls.Add(panelMain);
+            this.Name = "FormDiagnostic";
+            this.Text = "FormStatisticDiagnostic";
+            this.Activated += new System.EventHandler(this.FormDiagnostic_Activate);
+            //this.Deactivate += new System.EventHandler(this.FormDiagnostic_Deactivate);
+            this.Load += new System.EventHandler(this.FormDiagnostic_Load);            
+            this.ResumeLayout(false);
+        }
+
+        #endregion      
+    }
+}
+
---
+++ branches/diagnostic_006/StatisticDiagnostic1/FormDiagnostic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Drawing;
+using System.Text;
+using StatisticCommon;
+using System.Windows.Forms;
+using HClassLibrary;
+
+namespace StatisticDiagnostic1
+{
+    public partial class FormDiagnostic : FormMainBase
+    {
+        public FormDiagnostic()
+        {
+            InitializeComponent();
+            panelMain.SetDelegate(delegateStartWait, delegateStopWait, delegateEvent);
+        }
+
+        public void FormDiagnostic_Load(object obj, EventArgs ev)
+        {
+            panelMain.start();
+        }
+
+        public void FormDiagnostic_Close(object obj, EventArgs ev)
+        {
+            panelMain.Activate(false);
+            panelMain.Stop();
+        }
+
+        private void FormDiagnostic_Activate(object obj, EventArgs ev)
+        {
+            panelMain.Activate(true);
+        }
+
+        /*private void FormDiagnostic_Deactivate(object obj, EventArgs ev)
+        {
+            panelMain.Activate(false);
+        }*/
+    }
+
+}
