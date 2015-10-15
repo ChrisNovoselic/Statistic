20b4b0512f35d9a68e80364fea2dfca5786cf725
PasternakAS <PasternakAS@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2015-10-15 07:56:30 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@753 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/diagnostic_007/StatisticDiagnostic1/FormDiagnostic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Drawing;
+using System.Text;
+using StatisticCommon;
+using System.Windows.Forms;
+using HClassLibrary;
+
+namespace StatisticDiagnostic
+{
+    public partial class FormDiagnostic : FormMainBase
+    {
+        public FormDiagnostic()
+        {
+            InitializeComponent();
+        }
+
+        public void FormDiagnostic_Load(object obj, EventArgs ev)
+        {
+            panelMain.Start();
+        }
+
+        private void FormDiagnostic_Activate(object obj, EventArgs ev)
+        {
+            panelMain.Activate(true);
+        }
+
+        private void FormDiagnostic_Deactivate(object obj, EventArgs ev)
+        {
+            panelMain.Activate(false);
+        }
+    }
+
+}
---
+++ branches/diagnostic_007/StatisticTimeSync/FormStatisticTimeSync.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using HClassLibrary;
+
+namespace StatisticTimeSync
+{
+    public partial class FormStatisticTimeSync : Form //FormMainBaseWithStatusStrip
+    {
+        public FormStatisticTimeSync()
+        {
+            InitializeComponent(); 
+        }
+
+        /// <summary>
+        /// Запуск старта панели
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="ev"></param>
+        private void FormStatisticTimeSync_Load(object obj, EventArgs ev)
+        {
+            m_panelMain.Start();
+        }
+
+        /// <summary>
+        /// Активация формы
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="ev"></param>
+        private void FormStatisticTimeSync_Activate(object obj, EventArgs ev)
+        {
+           m_panelMain.Activate(true);
+        }
+
+        /// <summary>
+        /// Деактивация формы
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="ev"></param>
+        private void FormStatisticTimeSync_Deactivate(object obj, EventArgs ev)
+        {
+           m_panelMain.Activate(false);  
+        }
+    }
+}
