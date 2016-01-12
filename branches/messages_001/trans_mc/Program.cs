3446b75195c8ce22e220d4c08f3c0ec161ad405c
ApelgansAV <ApelgansAV@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2016-01-12 10:02:14 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@865 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/messages_001/trans_mc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using HClassLibrary;
+using StatisticCommon;
+using StatisticTrans;
+
+namespace trans_mc
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Logging.s_mode = Logging.LOG_MODE.UNKNOWN; //Если назначить неизвестный тип логирования - 1-е сообщения б. утеряны
+            //Logging.s_mode = Logging.LOG_MODE.DB;
+            Logging.s_mode = Logging.LOG_MODE.FILE_EXE;
+
+            if (RunOneInstance.ChekRunProgramm("trans_mc"))
+            {
+                return;
+            }
+
+            ProgramBase.Start();
+
+            FormMainTransMC formMain = null;
+
+            if (formMain == null)
+            {
+                try { formMain = new FormMainTransMC(); }
+                catch (Exception e)
+                {
+                    Logging.Logg().Exception(e, "Ошибка запуска приложения.", Logging.INDEX_MESSAGE.NOT_SET);
+                }
+            }
+
+            if (!(formMain == null))
+                Application.Run(formMain);
+
+            ProgramBase.Exit();
+        }
+    }
+}
