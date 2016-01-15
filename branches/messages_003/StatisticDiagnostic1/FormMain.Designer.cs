649681b6858728748eb2057661cd928c2ab6a0f5
ChrjapinAN <ChrjapinAN@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2016-01-15 06:56:28 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@876 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/messages_003/StatisticDiagnostic1/FormMain.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StatisticDiagnostic
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        PanelStatisticDiagnostic panelMain;
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            //this.panelMain = new PanelStatisticDiagnostic();
+            this.SuspendLayout();
+            //this.Controls.Add(panelMain); 
+            // 
+            // FormDiagnostic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 600);
+            this.Name = "FormDiagnostic";
+            this.Text = "FormStatisticDiagnostic";
+            this.MainMenuStrip = new MenuStrip();
+            this.MainMenuStrip.Items.AddRange(
+              new ToolStripMenuItem[] {
+                    new ToolStripMenuItem (@"Файл")
+                    , new ToolStripMenuItem (@"Настройка")
+                    , new ToolStripMenuItem (@"О программе")
+                }
+            );
+            (this.MainMenuStrip.Items[0] as ToolStripMenuItem).DropDownItems.Add(new ToolStripMenuItem(@"Выход"));
+            (this.MainMenuStrip.Items[0] as ToolStripMenuItem).DropDownItems[0].Click += new EventHandler(fMenuItemExit_Click);
+            (this.MainMenuStrip.Items[1] as ToolStripMenuItem).DropDownItems.Add(new ToolStripMenuItem(@"БД конфигурации"));
+            (this.MainMenuStrip.Items[1] as ToolStripMenuItem).DropDownItems[0].Click += new EventHandler(fMenuItemDBConfig_Click);
+            (this.MainMenuStrip.Items[2] as ToolStripMenuItem).Click += new EventHandler(fMenuItemAbout_Click);
+
+            this.Controls.Add(MainMenuStrip);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            this.Load += new System.EventHandler(this.FormDiagnostic_Load); 
+            this.Activated += new System.EventHandler(this.FormDiagnostic_Activate);
+            this.Deactivate += new System.EventHandler(this.FormDiagnostic_Deactivate);          
+        }
+
+        #endregion      
+    }
+}
+
---
+++ branches/messages_003/StatisticTrans/RunOneInstance.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+using StatisticTrans;
+using HClassLibrary;
+
+namespace StatisticTrans
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RunOneInstance
+    {
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern int ShowWindow(int hwnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern int GetWindow(int hwnd, int nCmdShow);
+
+        static Mutex _syncObject;
+        static readonly string AppPath = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+
+        /// <summary>
+        /// Событие - получение данных 
+        /// </summary>
+        public static event DelegateBoolFunc EvtRecievedTable;
+
+        /// <summary>
+        /// Находит запущенную копию приложения и разворачивает окно
+        /// </summary>
+        /// <param name="UniqueValue">уникальное значение для каждой программы (можно имя)</param>
+        /// <returns>true - если приложение было запущено</returns>
+        public static bool ChekRunProgramm(string UniqueValue)
+        {
+            bool applicationRun = false;
+            _syncObject = new Mutex(true, UniqueValue, out applicationRun);
+
+            if (!applicationRun)
+            {
+                //восстановить/развернуть окно                              
+                try
+                {
+                    Process[] procs = Process.GetProcessesByName(AppPath);
+                    MessageBox.Show("1111");
+
+                    foreach (Process proc in procs)
+                        if (proc.Id != Process.GetCurrentProcess().Id)
+                        {
+                            ShowWindow((int)proc.MainWindowHandle, 1);//нормально развернутое
+                            //ShowWindow((int)proc.MainWindowHandle, 3);//максимально развернутое
+                            SetForegroundWindow(proc.MainWindowHandle);
+                            GetWindow((int)proc.MainWindowHandle,1);
+                            EvtRecievedTable(!applicationRun);
+                            break;
+                        }
+                }
+                catch 
+                { return false; }
+            }
+         
+            return !applicationRun;
+        }
+
+    }
+}
