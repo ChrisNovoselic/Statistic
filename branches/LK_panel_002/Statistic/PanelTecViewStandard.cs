18cb8eea1d2b0e5319cf8e1300d4091c7665da2b
ChrjapinAN <ChrjapinAN@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2016-03-02 08:30:42 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@1064 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/LK_panel_002/Statistic/PanelTecViewStandard.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+//using System.ComponentModel;
+using System.Data;
+//using System.Data.SqlClient;
+using System.Drawing; //Color..
+using System.Threading;
+using System.Globalization;
+
+using ZedGraph;
+using GemBox.Spreadsheet;
+
+using HClassLibrary;
+using StatisticCommon;
+
+namespace Statistic
+{
+    public abstract class PanelTecViewStandard : PanelTecViewBase
+    {
+        public PanelTecViewStandard(TEC tec, int indx_tec, int indx_comp)
+            : base(TecView.TYPE_PANEL.VIEW, tec, indx_tec, indx_comp)
+        {
+        }
+
+        protected override void createPanelQuickData()
+        {
+            this._pnlQuickData = new PanelQuickDataStandard();
+        }
+
+        protected PanelQuickDataStandard PanelQuickData { get { return _pnlQuickData as PanelQuickDataStandard; } }
+    }
+}
