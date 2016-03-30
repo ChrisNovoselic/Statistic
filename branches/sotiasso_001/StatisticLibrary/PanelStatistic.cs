d2e5e5c083b92fc7f605334c454c962ac6314172
ChrjapinAN <ChrjapinAN@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2016-03-30 09:31:50 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@1133 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/sotiasso_001/StatisticLibrary/PanelStatistic.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing; //Color..
+
+using HClassLibrary;
+
+namespace StatisticCommon
+{
+    public abstract class PanelStatistic : HPanelCommon
+    {
+        protected DelegateFunc delegateStartWait;
+        protected DelegateFunc delegateStopWait;
+        protected DelegateFunc delegateEventUpdate;
+
+        public PanelStatistic(int cCols = -1, int cRows = -1)
+            : base(cCols, cRows)
+        {
+            Thread.CurrentThread.CurrentCulture =
+            Thread.CurrentThread.CurrentUICulture =
+                ProgramBase.ss_MainCultureInfo;
+        }        
+
+        public static volatile int POOL_TIME = -1
+            , ERROR_DELAY = -1;
+
+        public virtual void SetDelegateWait(DelegateFunc dStart, DelegateFunc dStop, DelegateFunc dStatus)
+        {
+            this.delegateStartWait = dStart;
+            this.delegateStopWait = dStop;
+            this.delegateEventUpdate = dStatus;
+        }
+
+        public abstract void SetDelegateReport(DelegateStringFunc ferr, DelegateStringFunc fwar, DelegateStringFunc fact, DelegateBoolFunc fclr);
+
+        public virtual bool MayToClose()
+        {
+            return true;
+        }
+
+        //protected List<TEC> m_listTEC;
+    }
+
+    public abstract class PanelContainerStatistic : PanelStatistic
+    {
+        private Type _controlType;
+
+        public PanelContainerStatistic(Type type)
+            : base()
+        {
+            registredControlType(type);
+        }
+
+        private void registredControlType(Type type)
+        {
+            _controlType = type;
+        }
+        /// <summary>
+        /// Старт опроса
+        /// </summary>
+        public override void Start()
+        {
+            base.Start();
+
+            foreach (Control ctrl in this.Controls)
+                if (ctrl.GetType().Equals(_controlType) == true)
+                    ((PanelStatistic)ctrl).Start();
+                else
+                    ;
+        }
+        /// <summary>
+        /// Остановка опроса
+        /// </summary>
+        public override void Stop()
+        {
+            foreach (Control ctrl in this.Controls)
+                if (ctrl.GetType().Equals(_controlType) == true)
+                    ((PanelStatistic)ctrl).Stop();
+                else
+                    ;
+
+            base.Stop();
+        }
+        /// <summary>
+        /// Активация панели
+        /// </summary>
+        /// <param name="active">Установка состояния панели</param>
+        /// <returns>Возвращает состояние после выполнения операции</returns>
+        public override bool Activate(bool active)
+        {
+            bool bRes = base.Activate(active);
+
+            if (bRes == false)
+                return bRes;
+            else
+                ;
+
+            int i = 0;
+            foreach (Control ctrl in this.Controls)
+                if (ctrl.GetType().Equals(_controlType) == true)
+                    ((PanelStatistic)ctrl).Activate(active);
+                else
+                    ;
+
+            return bRes;
+        }
+        /// <summary>
+        /// Определить размеры ячеек макета панели
+        /// </summary>
+        /// <param name="cols">Количество столбцов в макете</param>
+        /// <param name="rows">Количество строк в макете</param>
+        protected override void initializeLayoutStyle(int cols = -1, int rows = -1)
+        {
+            this.ColumnCount = cols;
+            if (this.ColumnCount == 0) this.ColumnCount++; else ;
+            this.RowCount = rows / this.ColumnCount;
+
+            initializeLayoutStyleEvenly();
+        }
+        /// <summary>
+        /// Назначить делегаты по отображению сообщений в строке статуса
+        /// </summary>
+        /// <param name="ferr">Делегат для отображения в строке статуса ошибки</param>
+        /// <param name="fwar">Делегат для отображения в строке статуса предупреждения</param>
+        /// <param name="fact">Делегат для отображения в строке статуса описания действия</param>
+        /// <param name="fclr">Делегат для удаления из строки статуса сообщений</param>
+        public override void SetDelegateReport(DelegateStringFunc ferr, DelegateStringFunc fwar, DelegateStringFunc fact, DelegateBoolFunc fclr)
+        {
+            foreach (Control ctrl in this.Controls)
+                if (ctrl.GetType().Equals(_controlType) == true)
+                    ((PanelStatistic)ctrl).SetDelegateReport(ferr, fwar, fact, fclr);
+                else
+                    ;
+        }
+    }
+
+    public abstract class PanelStatisticWithTableHourRows : PanelStatistic
+    {
+        protected abstract void initTableHourRows();
+    }
+}
---
+++ branches/sotiasso_001/StatisticTrans/ComponentTesting.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StatisticTrans;
+
+namespace StatisticTrans
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class ComponentTesting
+    {
+        /// <summary>
+        /// Кол-во пройденных итераций
+        /// </summary>
+        private int countIt = 0;
+
+        /// <summary>
+        /// Текущая иттерация
+        /// </summary>
+        private int currentIter;
+
+        private int commonSuccessIter
+            , commonTotalIter;
+
+        /// <summary>
+        /// Кол-во компонентов
+        /// </summary>
+        private string[] Iters;
+
+        /// <summary>
+        /// Счетчик нового дня
+        /// </summary>
+        public bool NextDay = false;
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="size">размер массива</param>
+        public ComponentTesting(int size)
+        {
+            Iters = new string[size];
+            currentIter =
+            commonSuccessIter =
+            commonTotalIter =
+                0;
+        }
+
+        /// <summary>
+        /// Успешные итерации
+        /// </summary>
+        /// <param name="nameElem">имя опрашеваемого компонента</param>
+        public void PushIter(string nameElem)
+        {
+            if (GetNum() == countIt)
+                ClearStck();
+
+            Iters[currentIter] = nameElem;
+            currentIter++;
+            commonSuccessIter++;
+            commonTotalIter++;
+            countIt++;
+            reportIter(getTextReportSuccessIter ());
+        }
+
+        /// <summary>
+        /// Кол-во итераций
+        /// </summary>
+        /// <returns>кол-во итераций</returns>
+        private int GetNum()
+        {
+            return Iters.Length;
+        }
+
+        /// <summary>
+        /// Уменьшение счетчика итераций
+        /// </summary>
+        public void PopIter()
+        {
+            reportIter(getTextReportErrorIter ());
+            currentIter--;
+            commonSuccessIter--;
+            reportIter(getTextReportSuccessIter());
+            countIt++;
+        }
+
+        private void reportIter(string text)
+        {
+            FormMainTrans.m_labelTime.Invoke(new Action(() => FormMainTrans.m_labelTime.Text = text));
+            FormMainTrans.m_labelTime.Invoke(new Action(() => FormMainTrans.m_labelTime.Update()));
+        }
+
+        /// <summary>
+        /// отчет об успешных итерациях
+        /// </summary>
+        private string getTextReportSuccessIter()
+        {
+            return "Время крайнего опроса: "
+                + DateTime.Now.ToString() + ";"
+                + " Успешных итераций: " + currentIter
+                    + "(" + commonSuccessIter + @")"
+                + " из " + GetNum()
+                    + @"(" + commonTotalIter + @")";
+        }
+
+        /// <summary>
+        /// Очистка итераций
+        /// </summary>
+        private void ClearStck()
+        {
+            currentIter = 0;
+            countIt = 0;
+        }
+
+        /// <summary>
+        /// отчет об ошибочных итерациях
+        /// </summary>
+        private string getTextReportErrorIter()
+        {
+            return getTextReportSuccessIter ()
+               + "; Ошибка на компоненте: " + Iters[currentIter].ToString();
+        }
+    }
+}
