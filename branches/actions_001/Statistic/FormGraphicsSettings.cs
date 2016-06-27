1391e5fe6d24f6d1fd7d1cc4a6a0fe74dcd6f9df
Apelgans_AV <Apelgans_AV@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2016-06-27 03:14:20 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@1170 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/actions_001/Statistic/FormGraphicsSettings.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Collections.Generic;
+//using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using HClassLibrary;
+using StatisticCommon;
+
+namespace Statistic
+{
+    public partial class FormGraphicsSettings : Form
+    {
+        public enum INDEX_COLOR
+        {
+            UDG, DIVIATION, ASKUE, SOTIASSO, REC, BG_ASKUE, BG_SOTIASSO, GRID
+                , COUNT_INDEX_COLOR
+        }
+
+        public enum TYPE_UPDATEGUI
+        {
+            SCALE, LINEAR, COLOR, SOURCE_DATA
+                , COUNT_TYPE_UPDATEGUI };
+
+        private enum CONN_SETT_TYPE {
+            AISKUE_PLUS_SOTIASSO
+            , AISKUE_3_MIN//, AISKUE_30_MIN
+            , SOTIASSO_3_MIN, SOTIASSO_1_MIN
+            , COSTUMIZE
+                , COUNT_CONN_SETT_TYPE
+        };
+        HMark m_markSourceData;
+
+        //public Color udgColor
+        //    , divColor
+        //    , pColor_ASKUE, pColor_SOTIASSO
+        //    , recColor
+        //    , m_bgColor_ASKUE, m_bgColor_SOTIASSO
+        //    , gridColor;
+        public bool scale;
+        public GraphTypes m_graphTypes;
+        public StatisticCommon.CONN_SETT_TYPE m_connSettType_SourceData;
+        private DelegateIntFunc delegateUpdateActiveGui;
+        private DelegateFunc delegateHideGraphicsSettings;
+        private FormMain m_formMain;
+
+        public enum GraphTypes
+        {
+            Linear,
+            Bar,
+        }
+
+        public FormGraphicsSettings(FormMain fm, DelegateIntFunc delUp, DelegateFunc Hide)
+        {
+            InitializeComponent();
+
+            delegateUpdateActiveGui = delUp;
+            delegateHideGraphicsSettings = Hide;
+            m_formMain = fm;
+
+            scale = false;
+            m_markSourceData = new HMark(0);
+
+            bool bGroupBoxSourceData = false;
+            CONN_SETT_TYPE cstGroupBoxSourceData = CONN_SETT_TYPE.AISKUE_3_MIN;
+            //Проверка условия прав доступа к возможности смены источника данных
+            if (HStatisticUsers.IsAllowed((int)HStatisticUsers.ID_ALLOWED.SOURCEDATA_CHANGED) == true)
+            //if (m_formMain.m_users.IsAllowed(HStatisticUsers.ID_ALLOWED.SOURCEDATA_CHANGED) == true)
+            //if ((HStatisticUsers.RoleIsAdmin == true) || (HStatisticUsers.RoleIsKomDisp == true))
+            {
+                bGroupBoxSourceData = true;
+                cstGroupBoxSourceData = CONN_SETT_TYPE.COSTUMIZE;
+
+                m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.AISKUE_PLUS_SOTIASSO].Enabled = HStatisticUsers.IsAllowed((int)HStatisticUsers.ID_ALLOWED.SOURCEDATA_ASKUE_PLUS_SOTIASSO);
+                m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.SOTIASSO_3_MIN].Enabled = HStatisticUsers.IsAllowed((int)HStatisticUsers.ID_ALLOWED.SOURCEDATA_SOTIASSO_3_MIN);
+            }
+            else
+                ;
+
+            this.groupBoxSourceData.Enabled = bGroupBoxSourceData;
+            m_markSourceData.Marked((int)cstGroupBoxSourceData);
+
+            checkedSourceData();
+
+            m_graphTypes = GraphTypes.Bar; //Гистограмма
+        }
+
+        private Color getForeColor (Color bgColor) {
+            return Color.FromArgb((bgColor.R + 128) % 256, (bgColor.G + 128) % 256, (bgColor.B + 128) % 256);
+        }
+
+        public Color COLOR(INDEX_COLOR indx)
+        {
+            return m_arlblColor [(int)indx].BackColor;
+        }
+
+        private void checkedSourceData()
+        {
+            m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.AISKUE_PLUS_SOTIASSO].Checked = m_markSourceData.IsMarked((int)(int)CONN_SETT_TYPE.AISKUE_PLUS_SOTIASSO);
+            m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.AISKUE_3_MIN].Checked = m_markSourceData.IsMarked((int)(int)CONN_SETT_TYPE.AISKUE_3_MIN);
+            m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.SOTIASSO_3_MIN].Checked = m_markSourceData.IsMarked((int)(int)CONN_SETT_TYPE.SOTIASSO_3_MIN);
+            m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.SOTIASSO_1_MIN].Checked = m_markSourceData.IsMarked((int)(int)CONN_SETT_TYPE.SOTIASSO_1_MIN);
+            m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.COSTUMIZE].Checked = m_markSourceData.IsMarked((int)(int)CONN_SETT_TYPE.COSTUMIZE);
+
+            if (m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.AISKUE_PLUS_SOTIASSO].Checked == true)
+                m_connSettType_SourceData = StatisticCommon.CONN_SETT_TYPE.DATA_AISKUE_PLUS_SOTIASSO;
+            else
+                if (m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.AISKUE_3_MIN].Checked == true)
+                    m_connSettType_SourceData = StatisticCommon.CONN_SETT_TYPE.DATA_AISKUE;
+                else
+                    if (m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.SOTIASSO_3_MIN].Checked == true)
+                        m_connSettType_SourceData = StatisticCommon.CONN_SETT_TYPE.DATA_SOTIASSO_3_MIN;
+                    else
+                        if (m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.SOTIASSO_1_MIN].Checked == true)
+                            m_connSettType_SourceData = StatisticCommon.CONN_SETT_TYPE.DATA_SOTIASSO_1_MIN;
+                        else
+                            if (m_arRadioButtonSourceData[(int)CONN_SETT_TYPE.COSTUMIZE].Checked == true)
+                                m_connSettType_SourceData = StatisticCommon.CONN_SETT_TYPE.COUNT_CONN_SETT_TYPE;
+                            else
+                                ;
+        }
+
+        private void cbxScale_CheckedChanged(object sender, EventArgs e)
+        {
+            scale = cbxScale.Checked;
+            delegateUpdateActiveGui((int)TYPE_UPDATEGUI.SCALE);
+        }
+
+        private void lbl_color_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Color = ((Label)sender).BackColor;
+            if (cd.ShowDialog(this) == DialogResult.OK)
+            {
+                ((Label)sender).BackColor = cd.Color;
+                ((Label)sender).ForeColor = Color.FromArgb((((Label)sender).BackColor.R + 128) % 256, (((Label)sender).BackColor.G + 128) % 256, (((Label)sender).BackColor.B + 128) % 256);
+                delegateUpdateActiveGui((int)TYPE_UPDATEGUI.COLOR);
+            }
+        }
+
+        private void GraphicsSettings_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            delegateHideGraphicsSettings();
+        }
+
+        public void SetScale()
+        {
+            cbxScale.Checked = !cbxScale.Checked;
+        }
+
+        private void rbtnLine_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnBar.Checked == true)
+                m_graphTypes = GraphTypes.Bar;
+            else
+                if (rbtnLine.Checked == true)
+                    m_graphTypes = GraphTypes.Linear;
+                else
+                    ;
+
+            delegateUpdateActiveGui((int)TYPE_UPDATEGUI.LINEAR);
+        }
+
+        private void rbtnSourceData_Click()
+        {
+            checkedSourceData();
+
+            delegateUpdateActiveGui((int)TYPE_UPDATEGUI.SOURCE_DATA);
+        }
+
+        private void rbtnSourceData_Click(CONN_SETT_TYPE indx)
+        {
+            if (m_arRadioButtonSourceData[(int)indx].Checked == false)
+            {
+                m_markSourceData.UnMarked();
+                m_markSourceData.Marked((int)indx);
+
+                rbtnSourceData_Click();
+            }
+            else
+                ;
+        }
+
+        private void rbtnSourceData_ASKUEPLUSSOTIASSO_Click(object sender, EventArgs e)
+        {
+            rbtnSourceData_Click(CONN_SETT_TYPE.AISKUE_PLUS_SOTIASSO);
+        }
+
+        private void rbtnSourceData_ASKUE_Click(object sender, EventArgs e)
+        {
+                rbtnSourceData_Click(CONN_SETT_TYPE.AISKUE_3_MIN);
+        }
+
+        private void rbtnSourceData_SOTIASSO3min_Click(object sender, EventArgs e)
+        {
+            rbtnSourceData_Click(CONN_SETT_TYPE.SOTIASSO_3_MIN);
+        }
+
+        private void rbtnSourceData_SOTIASSO1min_Click(object sender, EventArgs e)
+        {
+            rbtnSourceData_Click(CONN_SETT_TYPE.SOTIASSO_1_MIN);
+        }
+
+        private void rbtnSourceData_COSTUMIZE_Click(object sender, EventArgs e)
+        {
+            rbtnSourceData_Click(CONN_SETT_TYPE.COSTUMIZE);
+        }
+    }
+}
---
+++ branches/actions_001/Statistic/PanelAdminNSS.cs
@@ -0,0 +1,284 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+//using System.ComponentModel;
+using System.Data;
+using System.Security.Cryptography;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+
+using HClassLibrary;
+using StatisticCommon;
+
+namespace Statistic
+{
+    public partial class PanelAdminNSS : PanelAdmin
+    {
+        private System.Windows.Forms.Button btnImportExcel;
+        private System.Windows.Forms.Button btnExportExcel;
+        
+        protected override void InitializeComponents()
+        {
+            base.InitializeComponents();
+
+            this.btnImportExcel = new System.Windows.Forms.Button();
+            this.btnExportExcel = new System.Windows.Forms.Button();
+
+            this.m_panelManagement.Controls.Add(this.btnImportExcel);
+            this.m_panelManagement.Controls.Add(this.btnExportExcel);
+
+            int posY = 276;
+            // 
+            // btnImportExcel
+            // 
+            this.btnImportExcel.Location = new System.Drawing.Point(10, posY);
+            this.btnImportExcel.Name = "btnImportExcel";
+            this.btnImportExcel.Size = new System.Drawing.Size(154, m_iSizeY);
+            this.btnImportExcel.TabIndex = 667;
+            this.btnImportExcel.Text = "Импорт из Excel";
+            this.btnImportExcel.UseVisualStyleBackColor = true;
+            this.btnImportExcel.Click += new System.EventHandler(this.btnImportExcel_Click);
+            // 
+            // btnExportExcel
+            // 
+            //this.btnExportExcel.Location = new System.Drawing.Point(10, posY + 1 * (m_iSizeY + 2 * m_iMarginY));
+            this.btnExportExcel.Location = new System.Drawing.Point(10, posY + 1 * (m_iSizeY + m_iMarginY));
+            this.btnExportExcel.Name = "btnExportExcel";
+            this.btnExportExcel.Size = new System.Drawing.Size(154, m_iSizeY);
+            this.btnExportExcel.TabIndex = 668;
+            this.btnExportExcel.Text = "Экспорт в Excel";
+            this.btnExportExcel.UseVisualStyleBackColor = true;
+            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
+            this.btnExportExcel.Enabled = true;
+
+            this.dgwAdminTable = new DataGridViewAdminNSS();
+            this.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwAdminTable)).BeginInit();
+            this.m_panelRDGValues.Controls.Add(this.dgwAdminTable);
+            // 
+            // dgwAdminTable
+            //
+            this.dgwAdminTable.Location = new System.Drawing.Point(9, 9);
+            this.dgwAdminTable.Size = new System.Drawing.Size(574, 591);
+            this.dgwAdminTable.TabIndex = 1;
+            //this.dgwAdminTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwAdminTable_CellClick);
+            //this.dgwAdminTable.CellValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwAdminTable_CellValidated);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwAdminTable)).EndInit();
+            this.ResumeLayout();
+        }
+
+        public PanelAdminNSS(int idListener, HMark markQueries)
+            : base(idListener, FormChangeMode.MANAGER.NSS, markQueries, new int[] { 0, (int)TECComponent.ID.LK })
+        {
+            m_admin.SetDelegateSaveComplete(null);
+        }
+
+        private int GetIndexGTPOwner(int indx_tg)
+        {
+            int iRes = -1
+                , id_gtp_owner = ((DataGridViewAdminNSS)dgwAdminTable).GetIdGTPOwner(indx_tg);
+            
+            foreach (int indx in ((AdminTS_NSS)m_admin).m_listTECComponentIndexDetail)
+            {
+                if (m_admin.allTECComponents[indx].m_id == id_gtp_owner) {
+                    return ((AdminTS_NSS)m_admin).m_listTECComponentIndexDetail.IndexOf(indx);
+                }
+                else
+                    ;
+            }
+
+            return iRes;
+        }
+
+        protected override void getDataGridViewAdmin()
+        {
+            double value = 0.0;
+            bool valid = false;
+
+            foreach (int indx in ((AdminTS_NSS)m_admin).m_listTECComponentIndexDetail) 
+            {
+                if (m_admin.modeTECComponent(indx) == FormChangeMode.MODE_TECCOMPONENT.TG)
+                {
+                    int indx_tg = ((AdminTS_NSS)m_admin).m_listTECComponentIndexDetail.IndexOf(indx),
+                        indx_gtp = GetIndexGTPOwner(indx_tg);
+
+                    if ((!(indx_tg < 0)) && (!(indx_gtp < 0)))
+                        for (int i = 0; i < 24; i++)
+                        {
+                            ((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_tg][i].pbr = Convert.ToDouble(dgwAdminTable.Rows[i].Cells[indx_tg + 1].Value); // '+ 1' за счет DateTime
+                            //((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_tg][i].pmin =
+                            //((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_tg][i].pmax = 
+
+                            ((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_tg][i].recomendation = 0.0;
+                            ((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_tg][i].deviationPercent = ((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_gtp][i].deviationPercent;
+                            ((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_tg][i].deviation = ((AdminTS_NSS)m_admin).m_listCurRDGValues[indx_gtp][i].deviation;
+                        }
+                    else
+                        ;
+                }
+                else
+                    ;
+            }
+        }
+
+        private void addTextBoxColumn (DateTime date) {
+            int indx = ((AdminTS_NSS)m_admin).m_listTECComponentIndexDetail[this.dgwAdminTable.Columns.Count - 2];
+            ((DataGridViewAdminNSS)this.dgwAdminTable).addTextBoxColumn(m_admin.GetNameTECComponent(indx),
+                                                                        m_admin.GetIdTECComponent (indx),
+                                                                        m_admin.GetIdGTPOwnerTECComponent(indx),
+                                                                        date);
+
+            DataGridViewCellEventArgs ev;
+
+            for (int i = 0; i < 24; i++)
+            {
+                if (this.dgwAdminTable.Columns.Count == 3) //Только при добавлении 1-го столбца
+                    this.dgwAdminTable.Rows[i].Cells[0].Value = date.AddHours(i + 1).ToString(@"dd.MM.yyyy-HH", CultureInfo.InvariantCulture);
+                else
+                    ;
+
+                this.dgwAdminTable.Rows[i].Cells[this.dgwAdminTable.Columns.Count - 2].Value = ((AdminTS_NSS)m_admin).m_listCurRDGValues[this.dgwAdminTable.Columns.Count - 3][i].pbr.ToString("F2");
+                ev = new DataGridViewCellEventArgs(this.dgwAdminTable.Columns.Count - 2, i);
+                ((DataGridViewAdminNSS)this.dgwAdminTable).DataGridViewAdminNSS_CellValueChanged(null, ev);
+            }
+
+            m_admin.CopyCurToPrevRDGValues();
+
+            //this.dgwAdminTable.Invalidate();
+        }
+
+        private void updateTextBoxColumn()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+            }
+
+            //m_admin.CopyCurToPrevRDGValues();
+        }
+
+        public override void setDataGridViewAdmin(DateTime date)
+        {
+            //if (this.dgwAdminTable.Columns.Count < ((AdminTS_NSS)m_admin).m_listTECComponentIndexDetail.Count)
+                if (IsHandleCreated/*InvokeRequired*/ == true)
+                    this.BeginInvoke(new DelegateDateFunc(addTextBoxColumn), date);
+                else
+                    Logging.Logg().Error(@"PanelTecCurPower::setDataGridViewAdmin () - ... BeginInvoke (addTextBoxColumn) - ...", Logging.INDEX_MESSAGE.D_001);
+            //else
+            //    this.BeginInvoke(new DelegateFunc(updateTextBoxColumn));
+        }
+
+        public override void ClearTables()
+        {
+            ((DataGridViewAdminNSS)this.dgwAdminTable).ClearTables();
+        }
+
+        public override void InitializeComboBoxTecComponent(FormChangeMode.MODE_TECCOMPONENT mode)
+        {
+            base.InitializeComboBoxTecComponent (mode);
+
+            if (m_listTECComponentIndex.Count > 0) {
+                comboBoxTecComponent.Items.AddRange (((AdminTS_NSS)m_admin).GetListNameTEC ());
+
+                if (comboBoxTecComponent.Items.Count > 0)
+                {
+                    m_admin.indxTECComponents = m_listTECComponentIndex[0];
+                    comboBoxTecComponent.SelectedIndex = 0;
+                }
+                else
+                    ;
+            }
+            else
+                ;
+        }
+
+        public override bool Activate(bool active)
+        {
+            bool bRes = false;
+            
+            visibleControlRDGExcel();
+
+            if (active == true)
+                ; //m_admin.Resume ();
+            else {
+                //m_admin.Suspend();
+
+                //((DataGridViewAdminNSS)this.dgwAdminTable).ClearTables ();
+            }
+
+            bRes = base.Activate(active);
+
+            //ClearTables ();
+
+            //Вариант №1
+            //EventArgs.m_admin.ResetRDGExcelValues();
+            //base.comboBoxTecComponent_SelectionChangeCommitted(this, EventArgs.Empty);
+
+            //Вариант №2
+            //comboBoxTecComponent.SelectedIndex = prevSelectedIndex;
+
+            return bRes;
+        }
+
+        public override void Stop()
+        {
+            ClearTables ();
+
+            base.Stop ();
+        }
+
+        protected override void comboBoxTecComponent_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            base.comboBoxTecComponent_SelectionChangeCommitted (sender, e);
+
+            visibleControlRDGExcel();
+        }
+
+        private void visibleControlRDGExcel()
+        {
+            bool bImpExpButtonVisible = false;
+            if ((!(m_listTECComponentIndex == null))
+                && (m_listTECComponentIndex.Count > 0)
+                && (!(comboBoxTecComponent.SelectedIndex < 0))
+                && (m_admin.IsRDGExcel(m_listTECComponentIndex[comboBoxTecComponent.SelectedIndex]) == true))
+                bImpExpButtonVisible = true;
+            else
+                ;
+
+            btnImportExcel.Visible =
+            btnExportExcel.Visible =
+                bImpExpButtonVisible;
+        }
+
+        private void btnImportExcel_Click(object sender, EventArgs e)
+        {
+            ClearTables();
+
+            m_admin.ImpRDGExcelValues(m_listTECComponentIndex[comboBoxTecComponent.SelectedIndex], mcldrDate.SelectionStart);
+        }
+
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialog exportFolder = new FolderBrowserDialog();
+            //exportFolder.ShowDialog(this);
+
+            //if (exportFolder.SelectedPath.Length > 0) {
+                getDataGridViewAdmin();
+
+                Errors resultSaving = m_admin.ExpRDGExcelValues(m_listTECComponentIndex[comboBoxTecComponent.SelectedIndex], mcldrDate.SelectionStart);
+                if (resultSaving == Errors.NoError)
+                {
+                    btnRefresh.PerformClick ();
+                }
+                else
+                {
+                    if (resultSaving == Errors.InvalidValue)
+                        MessageBox.Show(this, "Изменение ретроспективы недопустимо!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    else
+                        MessageBox.Show(this, "Не удалось сохранить изменения, возможно отсутствует связь с базой данных.", "Ошибка сохранения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            //} else ;
+        }
+    }
+}
