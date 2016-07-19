5ca526094d6d3b17d15a416511b1f4a7879110d7
Apelgans_AV <Apelgans_AV@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2016-07-19 06:13:41 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@1198 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/bitec_tg_001/trans_mt/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+using HClassLibrary;
+using StatisticCommon;
+using StatisticTrans;
+
+namespace trans_mt
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Logging.s_mode = Logging.LOG_MODE.FILE_EXE;
+
+            ProgramBase.Start();
+
+            FormMainTransMT formMain = null;
+
+            try { formMain = new FormMainTransMT(); }
+            catch (Exception e)
+            {
+                Logging.Logg().Exception(e, "!Ошибка! запуска приложения.", Logging.INDEX_MESSAGE.NOT_SET);
+            }
+            try
+            {
+                if (!(formMain == null))
+                    Application.Run(formMain);
+                else
+                    ;
+            }
+            catch (Exception e)
+            {
+                Logging.Logg().Exception(e, "Ошибка выполнения приложения.", Logging.INDEX_MESSAGE.NOT_SET);
+            }
+
+            ProgramBase.Exit();
+        }
+    }
+}
