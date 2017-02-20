658d30db8565ffc6d4bf939fcc7dcf3fc71b27b2
LeontevAV <LeontevAV@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2017-02-20 02:53:57 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@1316 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/tsn_aiskue_002/StatisticTimeSync/FormMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StatisticTimeSync
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        PanelSourceData m_panelMain;
+
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            m_panelMain = new PanelSourceData();
+            
+            this.SuspendLayout();
+            // 
+            // FormStatisticTimeSync
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Name = "FormStatisticTimeSync";
+            this.Text = "StatisticTimeSync";
+            this.MainMenuStrip = new MenuStrip();
+            this.MainMenuStrip.Items.AddRange(
+              new ToolStripMenuItem[] {
+                    new ToolStripMenuItem (@"Файл")
+                    , new ToolStripMenuItem (@"Настройка")
+                    , new ToolStripMenuItem (@"О программе")
+                }
+            );
+            (this.MainMenuStrip.Items[0] as ToolStripMenuItem).DropDownItems.Add(new ToolStripMenuItem(@"Выход"));
+            (this.MainMenuStrip.Items[0] as ToolStripMenuItem).DropDownItems[0].Click += new EventHandler(fMenuItemExit_Click);
+            (this.MainMenuStrip.Items[1] as ToolStripMenuItem).DropDownItems.Add(new ToolStripMenuItem(@"БД конфигурации"));
+            (this.MainMenuStrip.Items[1] as ToolStripMenuItem).DropDownItems[0].Click += new EventHandler(fMenuItemDBConfig_Click);
+            (this.MainMenuStrip.Items[2] as ToolStripMenuItem).Click += new EventHandler(fMenuItemAbout_Click);
+
+            this.Controls.Add(MainMenuStrip);
+
+            //Создать панель для размещения "рабочих" панелей
+            Panel _panelMain = new Panel();
+            _panelMain.Location = new Point(0, this.MainMenuStrip.Height);
+            _panelMain.Size = new System.Drawing.Size(this.ClientSize.Width, this.ClientSize.Height - this.MainMenuStrip.Height - this.m_statusStripMain.Height);
+            _panelMain.Anchor = (AnchorStyles)(((AnchorStyles.Left | AnchorStyles.Top) | AnchorStyles.Right) | AnchorStyles.Bottom);
+            _panelMain.Controls.Add(this.m_panelMain);
+            this.Controls.Add(_panelMain);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            this.Load += new System.EventHandler(FormStatisticTimeSync_Load);
+            this.Activated += new System.EventHandler(FormStatisticTimeSync_Activate);
+            this.Deactivate += new System.EventHandler(FormStatisticTimeSync_Deactivate);
+        }
+
+        #endregion
+       
+    }
+}
+
