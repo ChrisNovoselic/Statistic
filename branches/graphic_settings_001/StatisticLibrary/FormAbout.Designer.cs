dd3a9dcc1b2c610876f31a5bab1eb60d646a8ea0
ShtyrenkovaSA <ShtyrenkovaSA@d8fff809-da14-dc41-87d1-cc2ed163d9e8>
2017-03-10 07:50:29 +0000

    git-svn-id: svn:///U:/Repos/statistic-techsite@1321 d8fff809-da14-dc41-87d1-cc2ed163d9e8

---
+++ branches/graphic_settings_001/StatisticLibrary/FormAbout.Designer.cs
@@ -0,0 +1,195 @@
+namespace StatisticCommon
+{
+    partial class FormAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.llblMailTo = new System.Windows.Forms.LinkLabel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.m_lblProductVersion = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.m_lblDomainMashineUserName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.m_lblCustomer = new System.Windows.Forms.Label();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(66, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 67);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Команда разработчиков:\r\nЛукашевич А.А.,\r\nКачайло Михаил,\r\nРевякин Е.А.,\r\nХряпин А" +
+    ".Н.";
+            // 
+            // llblMailTo
+            // 
+            this.llblMailTo.AutoSize = true;
+            this.llblMailTo.Location = new System.Drawing.Point(206, 82);
+            this.llblMailTo.Name = "llblMailTo";
+            this.llblMailTo.Size = new System.Drawing.Size(106, 13);
+            this.llblMailTo.TabIndex = 1;
+            this.llblMailTo.TabStop = true;
+            this.llblMailTo.Text = "small@groundzero.ru";
+            this.llblMailTo.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblMailTo_LinkClicked);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(145, 126);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(206, 95);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(128, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Revyakin_EA@sibeco.su";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblMailTo_LinkClicked);
+            // 
+            // m_lblProductVersion
+            // 
+            this.m_lblProductVersion.AutoSize = true;
+            this.m_lblProductVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m_lblProductVersion.Location = new System.Drawing.Point(66, 8);
+            this.m_lblProductVersion.Name = "m_lblProductVersion";
+            this.m_lblProductVersion.Size = new System.Drawing.Size(97, 13);
+            this.m_lblProductVersion.TabIndex = 4;
+            this.m_lblProductVersion.Text = "Product-Version";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(206, 108);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(122, 13);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Chrjapin_AN@sibeco.su";
+            // 
+            // m_lblDomainMashineUserName
+            // 
+            this.m_lblDomainMashineUserName.AutoSize = true;
+            this.m_lblDomainMashineUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m_lblDomainMashineUserName.ForeColor = System.Drawing.Color.Red;
+            this.m_lblDomainMashineUserName.Location = new System.Drawing.Point(66, 24);
+            this.m_lblDomainMashineUserName.Name = "m_lblDomainMashineUserName";
+            this.m_lblDomainMashineUserName.Size = new System.Drawing.Size(154, 13);
+            this.m_lblDomainMashineUserName.TabIndex = 6;
+            this.m_lblDomainMashineUserName.Text = "DomainMashineUserName";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(66, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Заказчик: ОАО \"Новосибирскэнерго\", отдел IT.";
+            // 
+            // m_lblCustomer
+            // 
+            this.m_lblCustomer.AutoSize = true;
+            this.m_lblCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m_lblCustomer.Location = new System.Drawing.Point(66, 42);
+            this.m_lblCustomer.Name = "m_lblCustomer";
+            this.m_lblCustomer.Size = new System.Drawing.Size(281, 13);
+            this.m_lblCustomer.TabIndex = 9;
+            this.m_lblCustomer.Text = "Заказчик: АО СибЭКо, Техническая дирекция";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(206, 71);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(144, 13);
+            this.linkLabel3.TabIndex = 10;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Lukashevich_AA@sibeco.su";
+            // 
+            // FormAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 155);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.m_lblCustomer);
+            this.Controls.Add(this.m_lblDomainMashineUserName);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.m_lblProductVersion);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.llblMailTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormAbout";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "О программе";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel llblMailTo;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label m_lblProductVersion;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label m_lblDomainMashineUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label m_lblCustomer;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
---
+++ branches/graphic_settings_001/TestFunc/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO; //Path
+using System.Data; //DataTable
+using System.Data.Common; //DbConnection
+
+using System.Data.OracleClient;
+using System.Data.OleDb;
+
+using System.ComponentModel;
+using System.Threading;
+
+using HClassLibrary;
+using StatisticCommon;
+
+namespace TestFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object test = null;
+
+            try {
+                //test = new DomainName ();
+                //test = new ImportCSV ();
+
+                //test = new HOracleConnection();
+                ////test = new HOleDbOracleConnection ();
+                //(test as HDbOracle).Open ();
+                //(test as HDbOracle).Query();                
+                //(test as HDbOracle).Close ();
+                //(test as HDbOracle).OutResult ();
+
+                //test = new DbSources();
+
+                test = new GenericSorted();
+            } catch (Exception e) {
+                Console.Write(e.Message + Environment.NewLine);
+            }
+
+            Console.Write(@"Для завершения работы нажмите любую клавишу..."); Console.ReadKey();
+        }
+
+        private class GenericSorted
+        {
+            public GenericSorted ()
+            {
+                DateTime dtRes;
+                List <DateTime> listDT = new List<DateTime> ();
+
+                listDT.Add (DateTime.Now);
+                listDT.Add(DateTime.Now.AddHours (-1));
+                listDT.Add(DateTime.Now.AddHours(1));
+
+                dtRes = listDT.FirstOrDefault ();
+
+                listDT.Sort ();
+                dtRes = listDT.LastOrDefault();
+            }
+        }
+
+        private class DbSources
+        {
+            private BackgroundWorker[] arThreadDbConn;
+            //private List<int> listListenerId = new List<int>();
+            private Semaphore m_semaConsole;
+            
+            public DbSources()
+            {
+                m_semaConsole = new Semaphore(1, 1);
+
+                ConnectionSettings[] arConnSett = new ConnectionSettings[]
+                {
+                    new ConnectionSettings (671, @"Statistic-CentreV", "10.100.104.18", "", 1433, @"techsite-2.X.X", @"client", @"client") 
+                    , new ConnectionSettings (63, @"Oracle", "10.220.2.5", "", 1521, @"ORCL", @"arch_viewer", @"1")
+                    , new ConnectionSettings (675, @"Statistic-CentreS", "10.100.204.63", "", 1433, @"techsite-2.X.X", @"client", @"client")
+                };
+
+                arThreadDbConn = new BackgroundWorker[arConnSett.Length];
+                int i = -1;
+                for (i = 0; i < arThreadDbConn.Length; i++)
+                {
+                    arThreadDbConn[i] = new BackgroundWorker();
+                    arThreadDbConn[i].DoWork += fThreadProc;
+                    arThreadDbConn[i].RunWorkerAsync(arConnSett[i]);
+                }
+
+                HClassLibrary.DbSources.Sources().UnRegister();
+            }
+
+            private void fThreadProc(object obj, DoWorkEventArgs ev)
+            {
+                int iListenerId = -1;
+                for (int j = 0; j < 16; j++)
+                {
+                    iListenerId = HClassLibrary.DbSources.Sources().Register(ev.Argument as ConnectionSettings, true, (ev.Argument as ConnectionSettings).name + @"-DbInterface");
+                    //listListenerId.Add(iListenerId);
+                    m_semaConsole.WaitOne();
+                    Console.WriteLine(@"ConnectionSettings.ID=" + (ev.Argument as ConnectionSettings).id + @", подписка.ID=" + iListenerId);
+                    m_semaConsole.Release(1);
+                }
+            }
+        }
+
+        private class DomainName {
+            public DomainName () {
+                string strMes = string.Empty;
+
+                strMes = @"Доменное имя пользователя: "; Console.Write(strMes);
+                try { strMes = Environment.UserDomainName; Console.Write(strMes + @"\"); }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+
+                try { strMes = Environment.UserName; Console.Write(strMes); }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+
+                Console.WriteLine();
+
+                strMes = @"IP: "; Console.Write(strMes);
+                strMes = string.Empty;
+                System.Net.IPAddress[] listIP = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName()).AddressList;
+                int indxIP = -1;
+                for (indxIP = 0; indxIP < listIP.Length; indxIP++)
+                {
+                    strMes += @"[" + indxIP + @"]=" + listIP[indxIP].ToString() + @", ";
+                }
+                strMes = strMes.Substring(0, strMes.Length - 2);
+                Console.WriteLine(strMes + Environment.NewLine);
+            }
+        }
+
+        private class ImportCSV {
+            public ImportCSV () {
+                DataTable m_tablePPBRValuesResponse = null;
+                string m_fullPathPPBRCSVValue = @"D:" //Path.GetDirectoryName(Environment.GetCommandLineArgs()[0])
+                     + @"\" + @"ГТП(генерация) Сессия(13) от 24.12.2014.csv";
+
+                int err = -1;                
+                string strPPBRCSVNameFileTemp = Path.GetFileNameWithoutExtension(m_fullPathPPBRCSVValue)
+                    , strMes = string.Empty;;
+
+                strPPBRCSVNameFileTemp = strPPBRCSVNameFileTemp.Replace("(", string.Empty);
+                strPPBRCSVNameFileTemp = strPPBRCSVNameFileTemp.Replace(")", string.Empty);
+                strPPBRCSVNameFileTemp = strPPBRCSVNameFileTemp.Replace(".", string.Empty);
+                strPPBRCSVNameFileTemp = strPPBRCSVNameFileTemp.Replace(" ", string.Empty);
+                strPPBRCSVNameFileTemp = Path.GetDirectoryName(m_fullPathPPBRCSVValue) + @"\" +
+                                        strPPBRCSVNameFileTemp +
+                                        Path.GetExtension(m_fullPathPPBRCSVValue);
+
+                ////при аргументе = каталог размещения наборов
+                //strPPBRCSVNameFile = m_PPBRCSVDirectory + strPPBRCSVNameFile + strCSVExt;
+                //strPPBRCSVNameFileTemp = m_PPBRCSVDirectory + strPPBRCSVNameFileTemp + strCSVExt;
+
+                //File.Copy(strPPBRCSVNameFile, strPPBRCSVNameFileTemp, true);
+                File.Copy(m_fullPathPPBRCSVValue, strPPBRCSVNameFileTemp, true);
+
+                StreamReader sr = new StreamReader(strPPBRCSVNameFileTemp);
+                string cont = sr.ReadToEnd().Replace(',', '.');
+                sr.Close(); sr.Dispose();
+                StreamWriter sw = new StreamWriter(strPPBRCSVNameFileTemp);
+                sw.Write(cont); sw.Flush(); sw.Close(); sw.Dispose();
+
+                if (!(m_tablePPBRValuesResponse == null)) m_tablePPBRValuesResponse.Clear(); else ;
+
+                if (strPPBRCSVNameFileTemp.Length > 0)
+                    m_tablePPBRValuesResponse = DbTSQLInterface.Select(@"CSV_DATASOURCE=" + Path.GetDirectoryName(strPPBRCSVNameFileTemp),
+                                                                            @"SELECT * FROM ["
+                        //+ @"Sheet1$"
+                                                                            + Path.GetFileName(strPPBRCSVNameFileTemp)
+                                                                            + @"]"
+                        //+ @" WHERE GTP_ID='" +
+                        //allTECComponents[indxTECComponents].name_future +
+                        //@"'"
+                                                                            , out err);
+                else
+                    ;
+
+                File.Delete(strPPBRCSVNameFileTemp);
+
+                int hour = -1;
+                double val = -1F;
+                HAdmin.RDGStruct  [] curRDGValues = new HAdmin.RDGStruct [24];
+                string name_future = @"GNOVOS02"
+                    , pbr_number = string.Empty;
+                //Получить значения для сохранения
+                DataRow[] rowsTECComponent = m_tablePPBRValuesResponse.Select(@"GTP_ID='" + name_future + @"'");
+                //Проверить наличие записей для ГТП
+                if (rowsTECComponent.Length > 0)
+                {
+                    foreach (DataRow r in rowsTECComponent)
+                    {
+                        hour = int.Parse(r[@"SESSION_INTERVAL"].ToString());
+
+                        if (double.TryParse(r[@"TotalBR"].ToString(), out val) == false)
+                            if (hour > 0)
+                                curRDGValues[hour].pbr = curRDGValues[hour - 1].pbr;
+                            else
+                                ;
+                        else
+                            curRDGValues[hour].pbr = val;
+
+                        curRDGValues[hour].pmin = double.Parse(r[@"PminBR"].ToString());
+                        curRDGValues[hour].pmax = double.Parse(r[@"PmaxBR"].ToString());
+
+                        strMes = String.Format(@"Час: {0}\tПБР={1}\tПБРмин={2}\tПБРмакс={3}",
+                                            hour.ToString(@"00"), curRDGValues[hour].pbr, curRDGValues[hour].pmin, curRDGValues[hour].pmax);
+                        Console.WriteLine (strMes);
+
+                        curRDGValues[hour].pbr_number = pbr_number;
+                    }
+                }
+                else
+                    ;
+            }
+        }
+    }
+}
